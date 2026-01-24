2fe5ff5da956453bb4bd75550bd1fdef6db9ced5
big l <jv071001@stu.provo.edu>
2026-01-24 12:12:05 -0700

    fix maybe

---
+++ 2048-merge-run/index.js
@@ -0,0 +1,23 @@
+((window.screenOrientation = "sensor_landscape"),
+  (window.resourceRoot =
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/"),
+  (window.resourceUrl =
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/"),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/libs/min/laya.core.min.js",
+  ),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/libs/min/laya.html.min.js",
+  ),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/libs/min/laya.ui.min.js",
+  ),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/libs/min/laya.d3.min.js",
+  ),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/libs/min/laya.physics3D.min.js",
+  ),
+  loadLib(
+    "https://cdn.jsdelivr.net/gh/genizy/brainrot@main/2048-merge-run/js/bundle.js",
+  ));
