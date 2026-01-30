cad07bc45424fb097a7d58ba99443c4ca842f3bb
big l <jv071001@stu.provo.edu>
2026-01-29 20:37:18 -0700

    fd

--- mindustry/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  if (url.pathname.endsWith('assets.txt')) {
-    event.respondWith(
-      fetch('https://cdn.jsdelivr.net/gh/bubbls/UGS-Assets@main/mindustry/assets/assets.txt')
-    );
-  }
-});
